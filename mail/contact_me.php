f1b36f52ac9255cf9b7600de16e5f10d3bea6198
Appdt-Dev <rabackuu@gmail.com>
2016-06-20 11:54:29 -0500

    Sitio Final gts
    Se agregaron nuevas fotos y se eliminaron paginas que no se usan

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,10 +16,10 @@ $subject = ($_POST['subject'] ? $_POST['subject'] : "Website Contact Form:  $nam
 
 
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'is_mike@outlook.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = $subject;
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nLast Name: $lastname\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@servicio-gts.mx\n"; // This is the email address the generated message will be from. We recommend using something like noreply@servicio-gts.mx.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 echo json_encode(array('success'=>'true'));
